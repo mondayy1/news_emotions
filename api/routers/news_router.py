642ab924d0df958be70324cb7b2f43e6cade3790
Hojoong Jang <hojoong310@gmail.com>
2024-10-19 20:13:59 +0900

    Add wordcounts in api

--- api/routers/news_router.py
+++ api/routers/news_router.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter
 from api.utils.news_crawler import *
 from api.utils.model import *
+from api.utils.word_cloud import *
 
 router = APIRouter()
 
 @router.get("/api/news/korean")
-def korean_news():
+def news_korean_emotions():
     news = get_korean_news()
     emotions = get_emotions(news['title'], news['content'])
-    return {"korean_news": news, "emotions": emotions}
+    wordcount = get_wordcount(news['title'], news['content'])
+    return {"korean_news": news, "emotions": emotions, "wordcount": wordcount}
+    
