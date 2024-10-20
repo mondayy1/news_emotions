0f1678ba67bc63d75d0edb326e15cdd8d84225f2
Hojoong Jang <hojoong310@gmail.com>
2024-10-21 00:39:18 +0900

    a

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-
-        {"role": "user", "content": "Who won the world series in 2020?"},
-        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
-        {"role": "user", "content": "Where was it played?"}
