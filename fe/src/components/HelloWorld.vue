11d4479753201f0f70688b3493fd52a470a92a4f
yhw1737 <yhw1737@naver.com>
2024-10-28 19:27:12 +0900

    api deleted as requested

--- fe/src/components/HelloWorld.vue
+++ fe/src/components/HelloWorld.vue
@@ -66,7 +66,7 @@ export default {
     },
     async fetchWordData() {
       try {
-        const response = await axios.get("http://203.255.92.239:10000/api/news/korean");
+        const response = await axios.get(".../api/news/korean");//api deleted as requested
         if (response && response.data) {
           const { summary, positives, negatives, wordcount } = response.data;
           this.summary = summary;
